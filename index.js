4f00a61a8fb7b89cade5424011afcfc9edc68dad
ShashikantLohar <shashilohar95@gmail.com>
2024-09-15 07:38:08 +0530

    lenear_search

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ let selection_sort=(arr)=>{
     }
 }
 
+let linear_search=(arr,target)=>{
+    let n=arr.length;
+    for(let i=0;i<n;i++){
+        if(arr[i]===target){
+            return true;
+        }
+    }
+    return false;
+}
